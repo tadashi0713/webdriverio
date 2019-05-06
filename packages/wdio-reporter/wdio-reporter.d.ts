348f01e834bbeced8084f7b9bc702667339a8899
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-05-06 17:37:34 +0900

    Add func type

--- packages/wdio-reporter/wdio-reporter.d.ts
+++ packages/wdio-reporter/wdio-reporter.d.ts
@@ -1,5 +1,20 @@
 declare class WDIOReporter {
-    constructor(options: any);
+    constructor(options?: any);
+
+    onRunnerStart(runner?: any): void;
+    onBeforeCommand(payload?: any): void;
+    onAfterCommand(payload?: any): void;
+    onScreenshot(): void;
+    onSuiteStart(params?: any): void;
+    onHookStart(hook?: any): void;
+    onHookEnd(hook?: any): void;
+    onTestStart(test?: any): void;
+    onTestPass(test?: any): void;
+    onTestFail(test?: any): void;
+    onTestSkip(test?: any): void;
+    onTestEnd(test?: any): void;
+    onSuiteEnd(suite?: any): void;
+    onRunnerEnd(runner?: any): void;
 }
 
 declare module "@wdio/reporter" {
